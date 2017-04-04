6838d225e7a9c7491f6026faacfadd96d73b0611
Anvay Srivastava <anvay.srivastava@flipkart.com>
2017-04-04 07:37:36 +0530

    Coded up nvidia architecture but it does not perform well

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ with open(data_path + 'driving_log.csv') as csv_file:
 center_images = []
 measurements = []
 
-correction = 0.1
+correction = 0.12
 for line in lines:
 	if ".jpg" in line[0]:	
 		image = cv2.imread(data_path + line[0].strip())
@@ -28,10 +28,14 @@ for line in lines:
 		left_steering = float(line[3]) + correction
 		center_images.append(left_image)
 		measurements.append(left_steering)
+		center_images.append(np.fliplr(image))
+		measurements.append(-left_steering)
 		image = cv2.imread(data_path + line[2].strip())
 		steering = float(line[3]) - correction
 		center_images.append(image)
 		measurements.append(steering)
+		center_images.append(np.fliplr(image))
+		measurements.append(-steering)
 X_train = np.array(center_images)
 Y_train = np.array(measurements)
 print(X_train[0].shape)
@@ -46,10 +50,11 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0-0.5, input_shape=(X_train[0].shape)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(1, 1, 1, border_mode='valid', subsample=(1, 10), activation='elu'))
-model.add(Convolution2D(3, 3, 3, border_mode='valid', activation='elu'))
-model.add(Convolution2D(6, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
-model.add(Convolution2D(16, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
+model.add(Convolution2D(24, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
+model.add(Convolution2D(36, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
+model.add(Convolution2D(48, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='elu'))
+model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='elu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
