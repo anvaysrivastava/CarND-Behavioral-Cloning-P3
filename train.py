d11cd2aff88b89c0abc7ff3769a4ba34084662af
Anvay Srivastava <anvay.srivastava@flipkart.com>
2017-04-18 07:49:20 +0530

    Adding more label and comments in code

--- train.py
+++ train.py
@@ -17,17 +17,23 @@ measurements = []
 
 correction = 0.1
 for line in lines:
-	if ".jpg" in line[0]:	
+	if ".jpg" in line[0]:
+		# Taking center image first
 		image = cv2.imread(data_path + line[0].strip())
-		center_images.append(image)
 		steering = float(line[3])
-		measurements.append(steering)
-		center_images.append(np.fliplr(image))
-		measurements.append(-steering)
+		if steering != 0:
+			center_images.append(image)
+			measurements.append(steering)
+			center_images.append(np.fliplr(image))
+			measurements.append(-steering)
+
+		# Taking left image
 		left_image = cv2.imread(data_path + line[1].strip())
 		left_steering = float(line[3]) + correction
 		center_images.append(left_image)
 		measurements.append(left_steering)
+
+		# Taking right image
 		image = cv2.imread(data_path + line[2].strip())
 		steering = float(line[3]) - correction
 		center_images.append(image)
@@ -41,22 +47,23 @@ print("Import of data complete")
 
 print("Trainig Started")
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D, Dropout
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0-0.5, input_shape=(X_train[0].shape)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(1, 1, 1, border_mode='valid', subsample=(1, 10), activation='elu'))
-model.add(Convolution2D(3, 3, 3, border_mode='valid', activation='elu'))
-model.add(Convolution2D(6, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
-model.add(Convolution2D(16, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
-model.add(Flatten())
+model.add(Cropping2D(cropping=((70,25),(0,0))))	#image = 320x65x3
+model.add(Convolution2D(3, 1, 1, border_mode='same')) # 320x65x3
+model.add(Convolution2D(6, 5, 1, border_mode='valid', activation='elu')) # 64x13x6
+model.add(Convolution2D(25, 3, 1, border_mode='valid', activation='elu')) # 31x4x25
+model.add(Convolution2D(20, 2, 1, border_mode='valid', activation='elu')) # 15x2x10
+model.add(Flatten()) # 200
+# model.add(Dropout(0.01))
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, Y_train, validation_split=0.20, shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
