877d1dac88ff65fe6583fbf1e19bb65ab93f2a9d
Anvay Srivastava <anvay.srivastava@flipkart.com>
2017-06-10 08:56:37 +0530

    Car goes fine on first bend and bridge. However on turn just before the dirt track it goes bonkers

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ print("Starting the import of data")
 import csv
 import cv2
 import numpy as np
+import random
 
 lines = []
 
@@ -14,16 +15,23 @@ with open(data_path + 'driving_log.csv') as csv_file:
 
 center_images = []
 measurements = []
+model_name = 'model.h5'
 
 correction = 0.1
 for line in lines:
-	if ".jpg" in line[0]:	
+	if ".jpg" in line[0]:
 		image = cv2.imread(data_path + line[0].strip())
-		center_images.append(image)
 		steering = float(line[3])
-		measurements.append(steering)
-		center_images.append(np.fliplr(image))
-		measurements.append(-steering)
+		if steering!=0:
+			center_images.append(image)
+			measurements.append(steering)
+			center_images.append(np.fliplr(image))
+			measurements.append(-steering)
+		else:
+			center_images.append(np.fliplr(image))
+			measurements.append(-steering)
+			center_images.append(image)
+			measurements.append(steering)
 		left_image = cv2.imread(data_path + line[1].strip())
 		left_steering = float(line[3]) + correction
 		center_images.append(left_image)
@@ -34,7 +42,7 @@ for line in lines:
 		measurements.append(steering)
 X_train = np.array(center_images)
 Y_train = np.array(measurements)
-print(X_train[0].shape)
+print(X_train[0].shape)	#160x320x3
 print("Import of data complete")
 
 #Train the data
@@ -45,7 +53,7 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 
 model = Sequential()
 model.add(Lambda(lambda x:x/255.0-0.5, input_shape=(X_train[0].shape)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Cropping2D(cropping=((70,25),(0,0)))) #65x320x3
 model.add(Convolution2D(1, 1, 1, border_mode='valid', subsample=(1, 10), activation='elu'))
 model.add(Convolution2D(3, 3, 3, border_mode='valid', activation='elu'))
 model.add(Convolution2D(6, 5, 5, border_mode='valid', subsample=(2, 2), activation='elu'))
@@ -57,6 +65,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
-
-model.save('model.h5')
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+print(model_name)
+model.save(model_name)
