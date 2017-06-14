dcc78c834049a8505044f87a3e3fe9f27ee7fb82
Anvay Srivastava <anvay.srivastava@flipkart.com>
2017-06-14 12:47:47 +0530

    Making the project ready for final submission

--- train.py
+++ train.py
@@ -1,6 +1,4 @@
 #Import the data
-data_path = "data/"
-print("Starting the import of data")
 import csv
 import cv2
 import numpy as np
@@ -8,19 +6,43 @@ import random
 
 lines = []
 
-with open(data_path + 'driving_log.csv') as csv_file:
+center_images = []
+measurements = []
+model_name = 'model.h5'
+data_path1 = "data/"
+data_path2 = "manual_data/"
+data_path3 = "smooth_driving/"
+data_path4 = "dirt_curve1/"
+
+
+print("Starting the import of data from data")
+with open(data_path1 + 'driving_log.csv') as csv_file:
 	reader = csv.reader(csv_file)
 	for line in reader:
 		lines.append(line)
 
-center_images = []
-measurements = []
-model_name = 'model.h5'
+print("Starting the import of data from manual_data")
+with open(data_path2 + 'driving_log.csv') as csv_file:
+	reader = csv.reader(csv_file)
+	for line in reader:
+		lines.append(line)
+
+print("Starting the import of data from smooth_driving")
+with open(data_path3 + 'driving_log.csv') as csv_file:
+	reader = csv.reader(csv_file)
+	for line in reader:
+		lines.append(line)
+
+print("Starting the import of data from smooth_driving")
+with open(data_path4 + 'driving_log.csv') as csv_file:
+	reader = csv.reader(csv_file)
+	for line in reader:
+		lines.append(line)
 
 correction = 0.5
 for line in lines:
 	if ".jpg" in line[0]:
-		image = cv2.imread(data_path + line[0].strip())
+		image = cv2.imread(line[0].strip())
 		steering = float(line[3])
 
 		center_images.append(np.fliplr(image))
@@ -29,13 +51,13 @@ for line in lines:
 		center_images.append(image)
 		measurements.append(steering)
 
-		left_image = cv2.imread(data_path + line[1].strip())
+		left_image = cv2.imread(line[1].strip())
 		left_steering = float(line[3]) + correction
 
 		center_images.append(left_image)
 		measurements.append(left_steering)
 
-		image = cv2.imread(data_path + line[2].strip())
+		image = cv2.imread(line[2].strip())
 		steering = float(line[3]) - correction
 
 		center_images.append(image)
@@ -52,23 +74,25 @@ print("Trainig Started")
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers.core import Dropout
 
 model = Sequential()
 # model.add(Lambda(lambda x:x/32.0-4.0, input_shape=(X_train[0].shape)))
 model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(X_train[0].shape))) #65x320x3
-model.add(Convolution2D(3, 3, 3, border_mode='valid', activation='elu'))
+model.add(Convolution2D(5, 3, 3, border_mode='valid', activation='elu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, border_mode='valid', activation='elu'))
+model.add(Convolution2D(8, 5, 5, border_mode='valid', activation='elu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(16, 5, 5, border_mode='valid', activation='elu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dropout(rate=0.1))
 model.add(Dense(200))
 model.add(Dense(100))
 model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=8)
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True, nb_epoch=9)
 print(model_name)
 model.save(model_name)
--- writeup.md
+++ writeup.md
@@ -1,9 +1,3 @@
-# CarND-Behavioral-Cloning-P3
-Udacity CarND Behavioral Cloning Project https://www.udacity.com/drive
-
-Template Repo: https://github.com/udacity/CarND-Behavioral-Cloning-P3
-
-
 **Behavioral Cloning Project**
 
 The goals / steps of this project are the following:
