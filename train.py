adad81a0c999df788dfae52e92035c25c2f8a060
Anvay Srivastava <anvay.srivastava@flipkart.com>
2017-06-23 14:46:43 +0530

    Sanitizing code

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-#Import the data
+#Import the dependencies
 import csv
 import cv2
 import numpy as np
@@ -13,48 +13,87 @@ import matplotlib.image as mpimg
 
 lines = []
 
+# Listing all the different datasets from which the model will be trained.
 model_name = 'model.h5'
 data_path1 = "data"
 data_path2 = "manual_data"
 data_path3 = "smooth_driving"
 data_path4 = "dirt_curve1"
 data_path5 = "dirt_curve2"
+
+# Correction factor for left and right images.
 correction = 0.5
 
+# Load the reference to all the images and steering.
+readCsv(data_path1)
+readCsv(data_path2)
+readCsv(data_path3)
+readCsv(data_path4)
+readCsv(data_path5)
 
+# Load the images and steering in memory and create an array out of it.
+# NOTE: While project requirement clearly stated that I should use generators.
+# I decided to go against it due to the following reasons:
+# 1) The data set is small 2.6GB and fits completely in memory.
+# 2) On my machine training time was ~16 times fasted when using model.fit
+# 	 compared to model.fit_generator
+X_train = np.array(list(chain(getCenterImage(), getFlippedImage(), getLeftImage(), getRightImage(), getFlippedLeftImage(), getFlippedRightImage())))
+Y_train = np.array(list(chain(getCenterSteering(), getFlippedSteering(), getLeftSteering(), getRightSteering(), getFlippedLeftSteering(), getFlippedRightSteering())))
+
+print("X Shape ", X_train.shape)
+print("Y Shape ", Y_train.shape)
+
+#Train the data
+
+print("Trainig Started")
+
+model = Sequential()
+#Cropping the image to avoid learning from environmental parameters like trees etc.
+model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160, 320, 3))) #65x320x3
+
+#Normalizing the input.
+#NOTE: I did not do X/256 - 0.5 because my car was performing terribly near
+# dirt tracks. My theory was that due to normalization network is not able to
+# differentiate the color of road and prefer grey over browm. However
+# doing X = X - 128.0 was a bit too extreme as the car would take sudden turns
+# on tree shadows before bridge.
+model.add(Lambda(lambda x:x/32.0-4.0))
+
+#Convolutional layer
+model.add(Conv2D(1, (1, 1), padding='valid', activation='elu'))
+model.add(Conv2D(3, (3, 3), padding='valid', activation='elu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(3, (3, 3), padding='valid', activation='elu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(6, (5, 5), padding='valid', activation='elu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (5, 5), padding='valid', activation='elu'))
+
+#Flatten layer.
+#NOTE: No need to add Dropout as I am doing only 2 epocs on non repeating
+# dataset. Hence I am fairly confident that the network has reliable redundancy
+# and is not overfitting.
+model.add(Flatten())
+
+# Fully connected layer.
+model.add(Dense(250))
+model.add(Dense(100))
+model.add(Dense(25))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, Y_train, validation_split=0.05, shuffle=True, epochs=2)
+
+#Save the model.
+model.save(model_name)
+
+# Helper functions.
 def readCsv(path):
 	print("Starting the import of data from ",path)
 	with open(path + '/driving_log.csv') as csv_file:
 		reader = csv.reader(csv_file)
 		for line in reader:
 			lines.append(line)
-
-def getData(batch_size):
-	images = np.empty([0, 160, 320, 3], dtype=float)
-	measurements = np.empty([0, ], dtype=float)
-	count = 0
-	for line in lines:
-		count += 1
-		if ".jpg" in line[0]:
-			if count<batch_size:
-				center_image = mpimg.imread(line[0].strip())
-				center_steering = float(line[3])
-				center_fliped_image = np.fliplr(center_image)
-
-				left_image = mpimg.imread(line[1].strip())
-				left_steering = float(line[3]) + correction
-				left_fliped_image = np.fliplr(left_image)
-
-				right_image = mpimg.imread(line[2].strip())
-				right_steering = float(line[3]) - correction
-				right_fliped_image = np.fliplr(right_image)
-
-
-				images = np.append(images,np.array([center_image, center_fliped_image, left_image, left_fliped_image, right_image, right_fliped_image]), axis = 0)
-				measurements = np.append(measurements,np.array([center_steering, -center_steering, left_steering, -left_steering, right_steering, -right_steering]),axis = 0)
-			else:
-				yield images, measurements
-	yield images, measurements
 
 def getCenterImage():
 	for line in lines:
@@ -115,60 +154,3 @@ def getFlippedRightSteering():
 	for line in lines:
 		if ".jpg" in line[0]:
 			yield -float(line[3]) + correction
-
-
-
-readCsv(data_path1)
-readCsv(data_path2)
-readCsv(data_path3)
-readCsv(data_path4)
-readCsv(data_path5)
-
-total_lines = len(lines)
-epochs = 6
-
-
-X_train = np.array(list(chain(getCenterImage(), getFlippedImage(), getLeftImage(), getRightImage(), getFlippedLeftImage(), getFlippedRightImage())))
-Y_train = np.array(list(chain(getCenterSteering(), getFlippedSteering(), getLeftSteering(), getRightSteering(), getFlippedLeftSteering(), getFlippedRightSteering())))
-print("X size ", X_train.shape)
-print("Y size ", Y_train.shape)
-print("Import of data complete")
-
-#Train the data
-
-print("Trainig Started")
-
-model = Sequential()
-model.add(Cropping2D(cropping=((70,25),(0,0)),input_shape=(160, 320, 3))) #65x320x3
-model.add(Lambda(lambda x:x/32.0-4.0))
-model.add(Conv2D(1, (1, 1), padding='valid', activation='elu'))
-# model.add(MaxPooling2D())
-model.add(Conv2D(3, (3, 3), padding='valid', activation='elu'))
-model.add(MaxPooling2D())
-# model.add(MaxPooling2D())
-# model.add(Dropout(rate=0.1))
-model.add(Conv2D(3, (3, 3), padding='valid', activation='elu'))
-model.add(MaxPooling2D())
-
-model.add(Conv2D(6, (5, 5), padding='valid', activation='elu'))
-model.add(MaxPooling2D())
-# model.add(MaxPooling2D())
-# model.add(Dropout(rate=0.1))
-model.add(Conv2D(16, (5, 5), padding='valid', activation='elu'))
-# model.add(MaxPooling2D())
-# model.add(MaxPooling2D())
-# model.add(MaxPooling2D())
-# model.add(Dropout(rate=0.1))
-model.add(Flatten())
-# model.add(Dropout(rate=0.1))
-model.add(Dense(250))
-model.add(Dense(100))
-model.add(Dense(25))
-model.add(Dense(1))
-# model.add(Dropout(rate=0.1))
-model.compile(loss='mse', optimizer='adam')
-
-# model.fit_generator(getData(10), steps_per_epoch=total_lines, epochs = epochs, initial_epoch = 0)
-model.fit(X_train, Y_train, validation_split=0.05, shuffle=True, epochs=2)
-print(model_name)
-model.save(model_name)
